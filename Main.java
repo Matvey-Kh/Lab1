61766ba2e05fba4fb52fdc9424f5dfe89a06b9ee
Matvey_Kh <Khudyakovmatvey2006@gmail.com>
2024-10-13 13:55:03 +0300

    1.3.0

--- Main.java
+++ Main.java
@@ -19,52 +19,52 @@ public class Main {
             // Арифмитическая прогрессия существует тогда, когда разности между двумя модулями пар чисел равны
             // В данной задаче сушествует всего 4 варианта комбинаций четырёх чисел по 3, а именно {A, B, C}, {A, C, D}, {B, C, D}, {A, B, D}
             // Проверяем тройку (A, B, C)
-            if ((Math.abs(A - B) == Math.abs(A - C)) || (Math.abs(B - C) == Math.abs(B - A)) || (Math.abs(C - A) == Math.abs(C - B))) { // Здесь существуют пары () 
-                int min = Math.min(A, Math.min(B, C));
-                int max = Math.max(A, Math.max(B, C));
-                int mid = A + B + C - min - max;
-                out.println(min + " " + mid + " " + max);
-                foundProgression = true;
+            if ((Math.abs(A - B) == Math.abs(A - C)) || (Math.abs(B - C) == Math.abs(B - A)) || (Math.abs(C - A) == Math.abs(C - B))) { // Сравнение всех модулей разности пар элементов 
+                int min = Math.min(A, Math.min(B, C)); // находение минимального элемента прогресии
+                int max = Math.max(A, Math.max(B, C)); // находение максимального элемента прогресии
+                int mid = A + B + C - min - max; // вычиселние среднего элемента прогрессии
+                out.println(min + " " + mid + " " + max); // вывод прогрессии из трёх элементов в порядке возрастания
+                foundProgression = true; // смена значения foundProgression с true на false (чтобы не выводилось "Здесь нет прогрессий!")
             }
             else {}
 
             // Проверяем тройку (A, C, D)
             if ((Math.abs(A - C) == Math.abs(A - D)) || (Math.abs(C - A) == Math.abs(C - D)) || (Math.abs(D - A) == Math.abs(D - C))) {
-                int min = Math.min(A, Math.min(C, D));
-                int max = Math.max(A, Math.max(C, D));
-                int mid = A + C + D - min - max;
-                out.println(min + " " + mid + " " + max);
-                foundProgression = true;
+                int min = Math.min(A, Math.min(C, D)); // находение минимального элемента прогресии
+                int max = Math.max(A, Math.max(C, D)); // находение максимального элемента прогресии
+                int mid = A + C + D - min - max; // вычиселние среднего элемента прогрессии
+                out.println(min + " " + mid + " " + max); // вывод прогрессии из трёх элементов в порядке возрастания
+                foundProgression = true; // смена значения foundProgression с true на false (чтобы не выводилось "Здесь нет прогрессий!")
             }
             else {}
 
             // Проверяем тройку (B, C, D)
             if ((Math.abs(B - C) == Math.abs(B - D)) || (Math.abs(C - B) == Math.abs(C - D)) || (Math.abs(D - B) == Math.abs(D - C))) {
-                int min = Math.min(B, Math.min(C, D));
-                int max = Math.max(B, Math.max(C, D));
-                int mid = B + C + D - min - max;
-                out.println(min + " " + mid + " " + max);
-                foundProgression = true;
+                int min = Math.min(B, Math.min(C, D)); // находение минимального элемента прогресии
+                int max = Math.max(B, Math.max(C, D)); // находение максимального элемента прогресии
+                int mid = B + C + D - min - max; // вычиселние среднего элемента прогрессии
+                out.println(min + " " + mid + " " + max); // вывод прогрессии из трёх элементов в порядке возрастания
+                foundProgression = true; // смена значения foundProgression с true на false (чтобы не выводилось "Здесь нет прогрессий!")
             }
             else {}
 
             // Проверяем тройку (A, B, D)
             if ((Math.abs(A - B) == Math.abs(A - D)) || (Math.abs(B - A) == Math.abs(B - D)) || (Math.abs(D - A) == Math.abs(D - B))) {
-                int min = Math.min(A, Math.min(B, D));
-                int max = Math.max(A, Math.max(B, D));
-                int mid = A + B + D - min - max;
-                out.println(min + " " + mid + " " + max);
-                foundProgression = true;
+                int min = Math.min(A, Math.min(B, D)); // находение минимального элемента прогресии
+                int max = Math.max(A, Math.max(B, D)); // находение максимального элемента прогресии
+                int mid = A + B + D - min - max; // вычиселние среднего элемента прогрессии
+                out.println(min + " " + mid + " " + max); // вывод прогрессии из трёх элементов в порядке возрастания
+                foundProgression = true; // смена значения foundProgression с true на false (чтобы не выводилось "Здесь нет прогрессий!")
             }
             else {}
 
-            if (foundProgression == false) {
-                out.print("Здесь нет прогрессий!");
+            if (foundProgression == false) { // Сравние значения foundProgression c его изначальным
+                out.print("Здесь нет прогрессий!"); // вывод, в случае если прогрессий нет
             }
             else {}
     }
     else {
-        out.print("Данные должны быть в виде натуральных чисел!");
+        out.print("Данные должны быть в виде натуральных чисел!"); // вывод, в случае если хотя бы одно из чисел не натуральное
     }
 }
 }
