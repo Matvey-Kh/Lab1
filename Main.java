4d5c4edbd6f9f29f4fd1e20776f69c53c98316c8
Matvey_Kh <Khudyakovmatvey2006@gmail.com>
2024-10-14 16:38:32 +0300

    1.9.0

--- Main.java
+++ Main.java
@@ -19,7 +19,8 @@ public class Main {
             // Арифмитическая прогрессия существует тогда, когда разности между двумя модулями пар чисел равны
             // В данной задаче сушествует всего 4 варианта комбинаций четырёх чисел по 3, а именно {A, B, C}, {A, C, D}, {B, C, D}, {A, B, D}
             // Проверяем тройку (A, B, C)
-            if ((Math.abs(A - B) == Math.abs(A - C)) || (Math.abs(B - C) == Math.abs(B - A)) || (Math.abs(C - A) == Math.abs(C - B))) { // Сравнение всех модулей разности пар элементов 
+            // Сравнение всех модулей разности пар элементов и проверка на отличие элементов тройки друг от друга
+            if (((Math.abs(A - B) == Math.abs(A - C)) || (Math.abs(B - C) == Math.abs(B - A)) || (Math.abs(C - A) == Math.abs(C - B))) & A != B & A != C & B != C) { 
                 int min = Math.min(A, Math.min(B, C)); // находение минимального элемента прогресии
                 int max = Math.max(A, Math.max(B, C)); // находение максимального элемента прогресии
                 int mid = A + B + C - min - max; // вычиселние среднего элемента прогрессии
@@ -29,7 +30,8 @@ public class Main {
             else {}
 
             // Проверяем тройку (A, C, D)
-            if ((Math.abs(A - C) == Math.abs(A - D)) || (Math.abs(C - A) == Math.abs(C - D)) || (Math.abs(D - A) == Math.abs(D - C))) {
+            // Сравнение всех модулей разности пар элементов и проверка на отличие элементов тройки друг от друга
+            if (((Math.abs(A - C) == Math.abs(A - D)) || (Math.abs(C - A) == Math.abs(C - D)) || (Math.abs(D - A) == Math.abs(D - C))) & A != C & A != D & C != D) {
                 int min = Math.min(A, Math.min(C, D)); // находение минимального элемента прогресии
                 int max = Math.max(A, Math.max(C, D)); // находение максимального элемента прогресии
                 int mid = A + C + D - min - max; // вычиселние среднего элемента прогрессии
@@ -39,7 +41,8 @@ public class Main {
             else {}
 
             // Проверяем тройку (B, C, D)
-            if ((Math.abs(B - C) == Math.abs(B - D)) || (Math.abs(C - B) == Math.abs(C - D)) || (Math.abs(D - B) == Math.abs(D - C))) {
+            // Сравнение всех модулей разности пар элементов и проверка на отличие элементов тройки друг от друга
+            if (((Math.abs(B - C) == Math.abs(B - D)) || (Math.abs(C - B) == Math.abs(C - D)) || (Math.abs(D - B) == Math.abs(D - C))) & B != C & B != D & C != D) {
                 int min = Math.min(B, Math.min(C, D)); // находение минимального элемента прогресии
                 int max = Math.max(B, Math.max(C, D)); // находение максимального элемента прогресии
                 int mid = B + C + D - min - max; // вычиселние среднего элемента прогрессии
@@ -49,7 +52,8 @@ public class Main {
             else {}
 
             // Проверяем тройку (A, B, D)
-            if ((Math.abs(A - B) == Math.abs(A - D)) || (Math.abs(B - A) == Math.abs(B - D)) || (Math.abs(D - A) == Math.abs(D - B))) {
+            // Сравнение всех модулей разности пар элементов и проверка на отличие элементов тройки друг от друга
+            if (((Math.abs(A - B) == Math.abs(A - D)) || (Math.abs(B - A) == Math.abs(B - D)) || (Math.abs(D - A) == Math.abs(D - B))) & A != B & A != D & B != D) {
                 int min = Math.min(A, Math.min(B, D)); // находение минимального элемента прогресии
                 int max = Math.max(A, Math.max(B, D)); // находение максимального элемента прогресии
                 int mid = A + B + D - min - max; // вычиселние среднего элемента прогрессии
@@ -59,7 +63,7 @@ public class Main {
             else {}
 
             if (foundProgression == false) { // Сравние значения foundProgression c его изначальным
-                out.print("Здесь нет прогрессий!"); // вывод, в случае если прогрессий нет
+                out.print("Здесь нет арифмитических прогрессий!"); // вывод, в случае если прогрессий нет
             }
             else {}
     }
